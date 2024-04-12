df7bfd9a30e228752c0da85586cc662930da7404
Matthew Malcomson <matthew.malcomson@arm.com>
2024-04-12 21:56:15 +0100

    Very small extra settings for orgmode

--- plugin/plugin_extras/orgmode.lua
+++ plugin/plugin_extras/orgmode.lua
@@ -1,1 +1,8 @@
-require('orgmode').setup({})
+-- Mostly want to just use the default mappings.
+require('orgmode').setup({
+	org_adapt_indentation = false,
+	org_edit_src_content_indentation = 2,
+})
+
+-- Currently have quite a big problem with slowness.  Not sure what the problem
+-- is.
