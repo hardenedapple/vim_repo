8fbcb96a3154281321dd931db40c754d278d5d32
Matthew Malcomson <matthew.malcomson@arm.com>
2024-05-28 13:22:05 +0100

    Ugly script added to update submodules
    Difference between this and `git submodule foreach` is that I need to
    record a mapping between which project uses `main` and which uses
    `master` (sigh ... differences).
    
    Could have approached it in another way, but copy/paste from command
    line and a few regexp search-replaces was the first way that came to
    mind.

---
+++ update-submodules.sh
@@ -0,0 +1,186 @@
+#!/bin/bash
+
+# Copy/pasted from some output and search-replaced into a valid set of actions,
+# so this is not pretty, but it at least gets the job done.
+cd ~/.vim
+
+pushd bundle/abolish
+git checkout master
+git pull
+popd
+pushd bundle/arduinosyntax
+git checkout master
+git pull
+popd
+pushd bundle/commentary
+git checkout master
+git pull
+popd
+pushd bundle/ctrlp
+git checkout master
+git pull
+popd
+pushd bundle/ctrlp-funky
+git checkout main
+git pull
+popd
+pushd bundle/dentures
+git checkout master
+git pull
+popd
+pushd bundle/dispatch
+git checkout master
+git pull
+popd
+pushd bundle/easygrep
+git checkout master
+git pull
+popd
+pushd bundle/eunuch
+git checkout master
+git pull
+popd
+pushd bundle/exchange
+git checkout master
+git pull
+popd
+pushd bundle/friendly-snippets
+git checkout main
+git pull
+popd
+pushd bundle/fugitive
+git checkout master
+git pull
+popd
+pushd bundle/gnupg
+git checkout master
+git pull
+popd
+pushd bundle/grepper
+git checkout master
+git pull
+popd
+pushd bundle/gv
+git checkout master
+git pull
+popd
+pushd bundle/lawrencium
+git checkout master
+git pull
+popd
+pushd bundle/linediff
+git checkout master
+git pull
+popd
+pushd bundle/neomake
+git checkout master
+git pull
+popd
+pushd bundle/nvim-lspconfig
+git checkout master
+git pull
+popd
+pushd bundle/nvim-snippets
+git checkout main
+git pull
+popd
+pushd bundle/nvim-treesitter
+git checkout master
+git pull
+popd
+pushd bundle/nvim-treesitter-context
+git checkout master
+git pull
+popd
+pushd bundle/nvim-treesitter-textobjects
+git checkout master
+git pull
+popd
+pushd bundle/obsession
+git checkout master
+git pull
+popd
+pushd bundle/orgmode
+git checkout master
+git pull
+popd
+pushd bundle/pathogen
+git checkout master
+git pull
+popd
+pushd bundle/plenary
+git checkout master
+git pull
+popd
+pushd bundle/repeat
+git checkout master
+git pull
+popd
+pushd bundle/sideways
+git checkout master
+git pull
+popd
+pushd bundle/sneak
+git checkout master
+git pull
+popd
+pushd bundle/snippets
+git checkout master
+git pull
+popd
+pushd bundle/submode
+git checkout master
+git pull
+popd
+pushd bundle/surround
+git checkout master
+git pull
+popd
+pushd bundle/tabular
+git checkout master
+git pull
+popd
+pushd bundle/telescope
+git checkout master
+git pull
+popd
+pushd bundle/telescope-arglist
+git checkout main
+git pull
+popd
+pushd bundle/telescope-fzy-native
+git checkout master
+git pull
+popd
+pushd bundle/telescope-project
+git checkout master
+git pull
+popd
+pushd bundle/telescope-ui-select
+git checkout master
+git pull
+popd
+pushd bundle/undotree
+git checkout master
+git pull
+popd
+pushd bundle/unimpaired
+git checkout master
+git pull
+popd
+pushd bundle/vimfindsme
+git checkout master
+git pull
+popd
+pushd bundle/vimple
+git checkout master
+git pull
+popd
+pushd bundle/visualstar
+git checkout master
+git pull
+popd
+pushd bundle/vsh
+git checkout master
+git pull
+popd
