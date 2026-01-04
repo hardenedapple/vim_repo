915439f40fee3fd2bf961a229d0692a10f7894a6
Matthew Malcomson <mmalcomson@nvidia.com>
2026-01-04 10:54:22 +0000

    Record my "turn hints off" thing in a comment
    Not sure by any means that my orgmode configuration file is the best
    place to put this, but it's where I've had it for a while and not
    attempting to change my muscle memory for fetching it right now.

--- plugin/plugin_extras/orgmode.lua
+++ plugin/plugin_extras/orgmode.lua
@@ -10,4 +10,15 @@ require('orgmode').setup({
 })
 
 -- Currently have quite a big problem with slowness.  Not sure what the problem
--- is.
+-- is.  Have adjusted a function directly in orgmode and that seems to have at
+-- least helped.
+--
+-- Also having a trouble with flickering syntax highlighting unless I set the
+-- following.  Not 100% sure it's a good idea, but at least seems to be
+-- improving things.
+-- lua vim.g._ts_force_sync_parsing = 1
+
+-- Doing some memory profiling in order to find the memory leak that is bugging
+-- me so much.
+-- require('jit.v').start()
+-- misc.memprof.start('/home/mmalcomson/mem-output.bin')
